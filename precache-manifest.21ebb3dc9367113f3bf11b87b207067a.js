accb77f98133ebf124a0ce2e7f6f6bbde98652dc
NathanW001 <nathan.wootton1@gmail.com>
2022-06-21 22:59:19 -0400

    Updates

---
+++ precache-manifest.21ebb3dc9367113f3bf11b87b207067a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "514aa9fe5653de1df78e",
+    "url": "/static/css/main.8f4c391e.chunk.css"
+  },
+  {
+    "revision": "514aa9fe5653de1df78e",
+    "url": "/static/js/main.514aa9fe.chunk.js"
+  },
+  {
+    "revision": "8b96d0460accf47cce3d",
+    "url": "/static/js/1.8b96d046.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6cdec61e2467109c9d76c4515bddb22c",
+    "url": "/static/media/nathan.6cdec61e.jpg"
+  },
+  {
+    "revision": "a1ea36a1b2d72eee7d01714e2b226167",
+    "url": "/index.html"
+  }
+];
